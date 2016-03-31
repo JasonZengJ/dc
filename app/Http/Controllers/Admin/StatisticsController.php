797fb0d1e5be564a1aa8709aca5c562fe2743790
JasonZeng <jason.zengjie@gmail.com>
2016-04-01 00:03:43 +0800

    增加每日营业额

--- app/Http/Controllers/Admin/StatisticsController.php
+++ app/Http/Controllers/Admin/StatisticsController.php
@@ -55,14 +55,11 @@ class StatisticsController extends Controller {
 
 
 		$year = Input::get('year');
-//		echo $year;
 		$months = ["一月","二月","三月","四月","五月","六月","七月","八月","九月","十月","十一月","十二月"];
 
 		$monthDatas = array();
 		for ($i = 0;$i < 12;$i++) {
 			$monthRange = $this->monthRange(Carbon::now()->startOfYear()->addMonth($i));
-
-//			$monthData = DB::select('SELECT sum(qiyu_order.order_price) as total_price FROM qiyu_order where order_addtime between ? and ? ',$monthRange);
 			$monthData = Orders::whereBetween('order_addtime',$monthRange)->sum('order_price');
 			array_push($monthDatas,$monthData * 1);
 		}
@@ -72,6 +69,31 @@ class StatisticsController extends Controller {
 
 	public function postTotalOrderPricePerDay() {
 
+		$month = Input::get('month');
+
+		$monthDate = Carbon::now()->startOfYear()->month($month);
+		$monthEndDay = $monthDate->endOfMonth()->day;
+
+		$days = array();
+		$dayDatas = array();
+		for ($i = 1;$i <= $monthEndDay;$i++) {
+
+			array_push($days,$i+"号");
+			$dayRanges = $this->dayRange($monthDate->addDay(1));
+			$dayData = Orders::whereBetween('order_addtime',$dayRanges)->sum('order_price');
+			array_push($dayDatas,$dayData * 1);
+
+		}
+
+		return array("days" => $days,"dayDatas" => $dayDatas);
+	}
+
+	function dayRange($date) {
+
+		$startDate = $date->startOfDay()->toDateTimeString();
+		$endDate   = $date->endOfDay()->toDateTimeString();
+
+		return array($startDate,$endDate);
 	}
 
 	function monthRange($date){
@@ -79,7 +101,7 @@ class StatisticsController extends Controller {
 		$timestamp=strtotime($date);
 		$mdays=date('t',$timestamp);
 
-		array_push($ret,Date('Y-m-1 00:00:00',$timestamp));
+		array_push($ret,Date('Y-m-01 00:00:00',$timestamp));
 		array_push($ret,Date('Y-m-'.$mdays.' 23:59:59',$timestamp));
 		return $ret;
 	}
--- resources/views/admin/statistics/statistics.blade.php
+++ resources/views/admin/statistics/statistics.blade.php
@@ -10,14 +10,17 @@
             color: #525252;
         }
 
+        .am-btn-secondary {
+            margin-left: 5%;
+        }
+
     </style>
-
-
 
     <div class="am-g container">
 
         <div class="am-scrollable-horizontal">
 
+            <button id="GetYearSales" type="button" class="am-btn am-btn-secondary">年度营业额</button>
             <div id="TotalPricePerMonth" style="width: auto;height:500px;"></div>
 
         </div>
@@ -42,16 +45,25 @@
         $(function(){
 
             var year = 2016;
-            $.post("{{url('/admin/statistics/dishes-amount-per-month')}}",{
-            },function(data) {
-                initDishSalesChart(data);
-            });
 
-            $.post("{{url('/admin/statistics/total-order-price-per-month')}}",{
-                year:year
-            },function(data) {
-                initSalesPriceChart(year + "年中每月的营业额","每月营业额",data);
-            });
+            loadYearTotalPrice();
+            loadYearSales();
+
+            $("#GetYearSales").click(loadYearTotalPrice);
+
+            function loadYearTotalPrice() {
+                $.post("{{url('/admin/statistics/total-order-price-per-month')}}",{
+                    year:year
+                },function(data) {
+                    initSalesPriceChart(year + "年中每月的营业额","每月营业额",data["months"],data["monthDatas"]);
+                });
+            }
+            function loadYearSales() {
+                $.post("{{url('/admin/statistics/dishes-amount-per-month')}}",{
+                },function(data) {
+                    initDishSalesChart(data);
+                });
+            }
 
             function initDishSalesChart(data) {
                 $('#DishStatistics').highcharts({
@@ -94,7 +106,9 @@
                 });
             }
 
-            function initSalesPriceChart(title,series_name,data) {
+
+
+            function initSalesPriceChart(title,series_name,categories,datas) {
                 $('#TotalPricePerMonth').highcharts({
                     chart: {
                         type: 'column'
@@ -103,7 +117,7 @@
                         text: title
                     },
                     xAxis: {
-                        categories:data["months"],
+                        categories:categories,
                         crosshair: true
                     },
                     yAxis: {
@@ -125,13 +139,30 @@
                         column: {
                             pointPadding: 0.2,
                             borderWidth: 0
+                        },
+                        series: {
+                            events: {
+                                click:function(e) {
+                                    loadMonthSales(e.point.index);
+                                }
+                            }
                         }
                     },
                     series: [{
                         name: series_name,
-                        data: data["monthDatas"],
+                        data: datas,
                     }]
                 });
+            }
+
+            function loadMonthSales(month) {
+
+                $.post("{{url('/admin/statistics/total-order-price-per-day')}}",{
+                    month:month
+                },function(data) {
+                    initSalesPriceChart((month + 1)+"月中每天的营业额","每日营业额",data["days"],data["dayDatas"]);
+                });
+
             }
 
         })
